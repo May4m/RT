d3f88dd8820e47d11eb02ea4a1d8ede13feabca1
Sizwe MAMBA <smamba@e5r1p7.jnb.42.fr>
2016-07-29 17:26:12 +0200

    RT

--- scene1.c
+++ scene1.c
@@ -7,7 +7,7 @@ t_spheres	*scene_one()
 	t_sphere	sp1	= new_sphere(new_vec3f(0, -10004, -20),
 									 new_vec3f(.3, .3, .3),
 									 new_vec3f(.1, .1, .1),
-									 100000, 1);
+									 100000, 0);
 	t_sphere    sp2 = new_sphere(new_vec3f(-4.0, 0, -50),
                                  new_color(.8, .8, .8),
                                  new_color(0., 0., 0.),
